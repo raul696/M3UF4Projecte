912226c49e92e87d4b46237d16fd30bfa0335996
Albert <albertbuch979@gmail.com>
2021-05-27 13:58:06 +0200

    getAllModels
    PlanesServices modelos hechos

--- src/PlanesService.java
+++ src/PlanesService.java
@@ -40,4 +40,18 @@ public class PlanesService {
         }
         return listPlane;
     }
+    public ArrayList<Model> getAllModels() {
+        ArrayList<Model> listModel = new ArrayList<>();
+        try {
+            Statement stmt= connection.createStatement();
+            ResultSet rs=stmt.executeQuery("SELECT * FROM models");
+            while(rs.next()){
+                Model m =new Model(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getShort(4),rs.getString(5),rs.getInt(6),rs.getShort(7),rs.getInt(8),rs.getInt(9),rs.getInt(10));
+                listModel.add(m);
+            }
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        return listModel;
+    }
 }
