3cb98898e05da801b8949516cab0599aaf52d649
DESKTOP-DUEGA0S\Admin <r.alen@sapalomera.cat>
2021-05-27 09:40:50 +0200

    posible EStructuta 0.3

---
+++ src/Plane.java
@@ -0,0 +1,47 @@
+
+public class Plane {
+    private int id;
+    private String registrationCode;
+    private String model;
+    private String mainColor;
+    private int hoursFlied;
+
+
+    public Plane(int id, String registrationCode, String model, String mainColor, int hoursFlied) {
+        this.id = id;
+        this.registrationCode = registrationCode;
+        this.model = model;
+        this.mainColor = mainColor;
+        this.hoursFlied = hoursFlied;
+    }
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "id=" + id +
+                ", registrationCode='" + registrationCode + '\'' +
+                ", model='" + model + '\'' +
+                ", mainColor='" + mainColor + '\'' +
+                ", hoursFlied=" + hoursFlied +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMainColor() {
+        return mainColor;
+    }
+
+    public int getHoursFlied() {
+        return hoursFlied;
+    }
+}
